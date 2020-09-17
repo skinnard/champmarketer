77b5dce8d9f5ab26c9a245ccd805685b50bf9022
skinnard <glenrhodges@gmail.com>
2020-09-17 06:14:53 -0400

    removed utils file as it was adding it captcha everywherer

--- js/mrare/index.js
+++ js/mrare/index.js
@@ -5,26 +5,13 @@ import mrCountdown from './countdown';
 import mrCountup from './countup';
 import mrDropdownGrid from './dropdown-grid';
 import './fade-page';
-import mrFlatpickr from './flatpickr';
-import './flickity';
 import mrFormEmail from './form-email';
-import mrIonRangeSlider from './ion-rangeslider';
-import mrIsotope from './isotope';
 import './jarallax';
-import mrMapsStyle from './maps-style';
-import mrMaps from './maps';
 import mrOverlayNav from './overlay-nav';
 import './navigation';
-import './plyr';
 import './popovers';
-import './prism';
-import mrReadingPosition from './reading-position';
 import mrSmoothScroll from './smooth-scroll';
-import mrSticky from './sticky';
 import './svg-injector';
-import mrTwitterFetcher from './twitter-fetcher';
-import mrTypedText from './typed-text';
-import mrUtil from './util';
 
 (() => {
   if (typeof $ === 'undefined') {
@@ -36,17 +23,7 @@ export {
   mrCountdown,
   mrCountup,
   mrDropdownGrid,
-  mrFlatpickr,
   mrFormEmail,
-  mrIonRangeSlider,
-  mrIsotope,
-  mrMapsStyle,
-  mrMaps,
   mrOverlayNav,
-  mrReadingPosition,
   mrSmoothScroll,
-  mrSticky,
-  mrTwitterFetcher,
-  mrTypedText,
-  mrUtil,
 };
--- js/mrare/util.js
+++ js/mrare/util.js
@@ -118,7 +118,6 @@ const mrUtil = (($) => {
     },
   };
 
-
   return Util;
 })(jQuery);
 
